db499437cf36f97407b584eb5a865f73930d2092
MashPlant <740678788@qq.com>
2019-06-21 08:16:33 +0800

    bug fix: forget to minimize before merging

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub mod dfa;
 pub fn re2dfa<I: IntoIterator<Item=S>, S: AsRef<str>>(res: I) -> Result<(dfa::Dfa, [u8; 128]), String> {
   let mut dfas = Vec::new();
   for (id, re) in res.into_iter().enumerate() {
-    dfas.push(dfa::Dfa::from_nfa(&nfa::Nfa::from_re(&re::parse(re.as_ref())?), id as u32));
+    dfas.push(dfa::Dfa::from_nfa(&nfa::Nfa::from_re(&re::parse(re.as_ref())?), id as u32).minimize());
   }
   let dfa = dfa::Dfa::merge(&dfas);
   let ec = compress::ec_of_dfas(&[&dfa]);
